9c835b46ca18e43a6b092a70405fee4713b7c733
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2023-03-27 21:09:58 +0100

    Added simple PubSub system

---
+++ pubsub/message.go
@@ -0,0 +1,24 @@
+package pubsub
+
+type Message struct {
+	topic string
+	body  string
+}
+
+func NewMessage(msg string, topic string) *Message {
+	// Returns the message object
+	return &Message{
+		topic: topic,
+		body:  msg,
+	}
+}
+
+func (m *Message) GetTopic() string {
+	// Returns the topic of the message
+	return m.topic
+}
+
+func (m *Message) GetMessageBody() string {
+	// Returns the message body.
+	return m.body
+}
